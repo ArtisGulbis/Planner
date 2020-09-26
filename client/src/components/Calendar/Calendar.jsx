d4e7c02cbdd75271b18a42465d97ca59b4e5c731
Artis <artux28@hotmail.com>
2020-09-26 10:49:00 +0200

    hide passed days toggle switch

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './calendar.styles.scss';
 import Card from '../Card/Card';
 import { setData } from '../../redux/month/month.actions';
 import { v4 } from 'uuid';
+import moment from 'moment';
 
 const Calendar = () => {
+  const currentDay = moment().format('DD');
   const dispatch = useDispatch();
+  const [hideCards, setHideCards] = useState(false);
   const monthName = useSelector((state) => state.month.currentMonth.monthName);
   const totalPoints = useSelector(
     (state) => state.month.currentMonth.totalPoints
@@ -21,9 +24,25 @@ const Calendar = () => {
     <div className='calendar-container'>
       <h1 className='month-name'>{monthName}</h1>
       <h2>{totalPoints}</h2>
+      <label className='switch'>
+        <input
+          type='checkbox'
+          onChange={(e) => setHideCards(!hideCards)}
+        ></input>
+        <span className='slider round'></span>
+      </label>
       <div className='card-container' key={v4()}>
         {days.map((day, i) => {
-          return (
+          return !hideCards ? (
+            <Card
+              key={i}
+              monthDayNumber={day.dayNr}
+              tasks={day.tasks}
+              totalPoints={day.totalPoints}
+            ></Card>
+          ) : hideCards && parseInt(day.dayNr) < parseInt(currentDay) ? (
+            ''
+          ) : (
             <Card
               key={i}
               monthDayNumber={day.dayNr}
